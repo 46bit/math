b880802163440b683eb0b8f3137e6277c1631768
Michael Mokrysz <hi@46b.it>
2018-01-17 21:38:58 +0000

    [WIP] Emit object files

--- src/math/compiler/mod.rs
+++ src/math/compiler/mod.rs
@@ -6,6 +6,11 @@ use self::function::*;
 use self::expression::*;
 use llvm;
 use llvm::prelude::*;
+use std::fs::File;
+use std::io::prelude::*;
+use std::mem;
+use std::ptr;
+use std::slice;
 use std::ffi::{CStr, CString};
 use std::collections::HashMap;
 
@@ -13,19 +18,70 @@ use std::collections::HashMap;
 pub enum Error {}
 
 pub unsafe fn compile(statements: &Statements) -> Result<String, Error> {
-    let (ctx, module) = synthesise(&statements.0)?;
+    let (llvm_ctx, llvm_module) = synthesise(&statements.0)?;
 
-    let c = llvm::core::LLVMPrintModuleToString(module);
+    let c = llvm::core::LLVMPrintModuleToString(llvm_module);
     let llvm_ir = CStr::from_ptr(c).to_string_lossy().into_owned();
     llvm::core::LLVMDisposeMessage(c);
 
-    llvm::core::LLVMDisposeModule(module);
-    llvm::core::LLVMContextDispose(ctx);
+    let mut out_file = File::create("a.o").unwrap();
+    emit(llvm_module, &mut out_file);
+
+    llvm::core::LLVMDisposeModule(llvm_module);
+    llvm::core::LLVMContextDispose(llvm_ctx);
     // FIXME: Error handling.
     return Ok(llvm_ir);
 }
 
-pub unsafe fn synthesise(
+unsafe fn emit<W>(llvm_module: LLVMModuleRef, mut out: W)
+where
+    W: Write,
+{
+    llvm::target::LLVM_InitializeNativeTarget();
+    llvm::target::LLVM_InitializeNativeAsmPrinter();
+    llvm::target::LLVM_InitializeNativeAsmParser();
+
+    let llvm_triple = CString::from_raw(llvm::target_machine::LLVMGetDefaultTargetTriple());
+    let mut llvm_target = mem::uninitialized();
+    assert_eq!(
+        llvm::target_machine::LLVMGetTargetFromTriple(
+            llvm_triple.as_ptr(),
+            &mut llvm_target,
+            ptr::null_mut(),
+        ),
+        0
+    );
+
+    let llvm_empty_name = llvm_name("");
+    let llvm_target_machine = llvm::target_machine::LLVMCreateTargetMachine(
+        llvm_target,
+        llvm_triple.as_ptr(),
+        llvm_empty_name.as_ptr(),
+        llvm_empty_name.as_ptr(),
+        llvm::target_machine::LLVMCodeGenOptLevel::LLVMCodeGenLevelAggressive,
+        llvm::target_machine::LLVMRelocMode::LLVMRelocDefault,
+        llvm::target_machine::LLVMCodeModel::LLVMCodeModelDefault,
+    );
+
+    let mut llvm_mem_buf: LLVMMemoryBufferRef = mem::uninitialized();
+    llvm::target_machine::LLVMTargetMachineEmitToMemoryBuffer(
+        llvm_target_machine,
+        llvm_module,
+        llvm::target_machine::LLVMCodeGenFileType::LLVMObjectFile,
+        ptr::null_mut(),
+        &mut llvm_mem_buf,
+    );
+
+    let llvm_out = slice::from_raw_parts(
+        llvm::core::LLVMGetBufferStart(llvm_mem_buf) as *const _,
+        llvm::core::LLVMGetBufferSize(llvm_mem_buf) as usize,
+    );
+    out.write_all(llvm_out).unwrap();
+    llvm::core::LLVMDisposeMemoryBuffer(llvm_mem_buf);
+    llvm::target_machine::LLVMDisposeTargetMachine(llvm_target_machine);
+}
+
+unsafe fn synthesise(
     statements: &Vec<Statement>,
 ) -> Result<(*mut llvm::LLVMContext, LLVMModuleRef), Error> {
     let ctx = llvm::core::LLVMContextCreate();
